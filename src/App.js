42290a742f318759be82187443aa37f51e29918d
ickejohannes <ickejohannes@gmail.com>
2022-06-14 12:41:08 +0200

    Do iteration 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useState } from 'react';
 
 const firstFive = allContacts.slice(0, 5);
 
+function test() {
+  return "hi!"
+}
+
 function App() {
   const [contacts, setContacts] = useState(firstFive);
 
@@ -15,6 +19,8 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
           </tr>
         </thead>
         <tbody>
@@ -24,6 +30,10 @@ function App() {
                 <td><img src={contact.pictureUrl} alt={contact.name} className="actorPic"/></td>
                 <td>{contact.name}</td> 
                 <td>{contact.popularity}</td>
+                {contact.wonOscar && <td>🏆</td>}
+                {contact.wonEmmy && <td>🏆</td>}
+
+                
               </tr>
             )
           })}
